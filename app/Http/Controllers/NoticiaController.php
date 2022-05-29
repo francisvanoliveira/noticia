6cf2180a6cbaf70a6bb0f99597718714cbfb9793
Francisvan Oliveira <francisvan.oliveira@francisvanoliveira.com.br>
2022-05-28 20:32:08 -0400

    :package: CRUD concluído

--- app/Http/Controllers/NoticiaController.php
+++ app/Http/Controllers/NoticiaController.php
@@ -34,19 +34,35 @@ class NoticiaController extends Controller
         return redirect()->back()->with('mensagem', 'Registro criado com sucesso!');
     }
 
-    public function edit()
+    public function edit($noticia)
     {
+        $noticia = Noticia::findOrFail($noticia);
 
+        return view('noticias.edit', [
+            'noticia' => $noticia
+        ]);
     }
 
-    public function update()
+    public function update($noticia, Request $request)
     {
-
-    }
-
-    public function destroy()
-    {
+        $noticia = Noticia::findOrFail($noticia);
+        $dados = $request->all();
+        $dados['data_publicacao'] = Carbon::createFromFormat("d/m/Y", $dados['data_publicacao'])->format("Y-m-d");
+        if ($request->imagem) {
+            $request->imagem->storeAs('public', $request->imagem->getClientOriginalName());
+            $dados['imagem'] = '/storage/' . $request->imagem->getClientOriginalName();
+        }
+        $noticia->update($dados);
         
+        return redirect()->back()->with('mensagem', 'Registro atualizado com sucesso!');
+    }
+
+    public function destroy($noticia)
+    {
+        $noticia = Noticia::findOrFail($noticia);
+        $noticia->delete();
+
+        return redirect('/noticias')->with('mensagem', 'Registro excluído com sucesso!');
     }
     
 }
---
+++ resources/views/noticias/edit.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário de Notícias</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css" integrity="sha512-mSYUmp1HYZDFaVKK//63EcZq4iFWFjxSL+Z3T/aCt4IO9Cejm03q3NKKYN6pFQzY0SBOr8h+eCIAZHPXcpZaNw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+
+    <div class="container pt-5">
+
+        @if(session()->has('mensagem'))
+            <div class="alert alert-success">
+                {{ session()->get('mensagem')}}
+            </div>
+        @endif
+
+        <form action="/noticias/{{ $noticia->id }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+
+            <div class="form-group">
+                <label for="conteudo">Conteúdo</label>
+                <textarea name="conteudo" placeholder="Digite o conteúdo da notícia" class="form-control" rows="5">{{ $noticia->conteudo }}</textarea>
+            </div>
+
+            <div class="form-group">
+                <label for="status">Status</label>
+                <select name="status" class="form-control">
+                    <option value="A" {{ $noticia->status == "A" ? "selected='selected'" : "" }}>Ativo</option>
+                    <option value="I" {{ $noticia->status == "I" ? "selected='selected'" : "" }}>Inativo</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="data_publicacao">Data da Publicação</label>
+                <input type="text" name="data_publicacao" class="form-control" data-provide="datepicker" data-date-language="pt-BR" value="{{ Carbon\Carbon::createFromFormat("Y-m-d" , $noticia->data_publicacao)->format("d/m/Y") }}">
+            </div>
+
+            <button type="submit" class="btn btn-primary">Salvar</button>
+        </form><!--fim formulario-->
+        <a href="/noticias" class="btn btn-light">Voltar</a>
+    </div><!--fim div container-->
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js" integrity="sha512-T/tUfKSV1bihCnd+MxKD0Hm1uBBroVYBOYSk1knyvQ9VyZJpc/ALb4P0r6ubwVPSGB2GvjeoMAJJImBG12TiaQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/locales/bootstrap-datepicker.pt-BR.min.js" integrity="sha512-mVkLPLQVfOWLRlC2ZJuyX5+0XrTlbW2cyAwyqgPkLGxhoaHNSWesYMlcUjX8X+k45YB8q90s88O7sos86636NQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+</html>
